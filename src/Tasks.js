0fcd219cb69ed76b8e82cc334017e9e78e00e4ac
jblavisse <jean-baptiste@jblavisse.fr>
2021-07-13 17:16:26 +0200

    Logout and login now works without reloading

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Tasks() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        if(window.localStorage.getItem('user')) {
+            // Dans le cas où il y a un token
+            console.log("Je suis connecté");
+            setIsLoggedIn(true);
+        }
+        else {
+            // Dans le cas où on n'a pas de token
+            console.log("MEH!");
+            setIsLoggedIn(false);
+        }
+    }, [])
   
     const handleSubmit = (e) => {
       e.preventDefault();
@@ -14,10 +28,22 @@ export default function Tasks() {
       .then((res) => {
         console.log(res);
         window.localStorage.setItem('user',res.data.accessToken)
+        setIsLoggedIn(true);
       })
     }
 
-    return (
+    const logout = () => {
+        window.localStorage.removeItem('user');
+        setIsLoggedIn(false);
+    }
+
+    if(isLoggedIn) {
+        return (<div>
+            <h1>Je suis connecté!</h1>
+            <button onClick={logout}>Se déconnecter</button>
+        </div>)
+    }
+    else return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="email" 
